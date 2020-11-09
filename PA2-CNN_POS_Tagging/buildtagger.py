83c116d086b5266ff69ed53fd9958a6ca6e5222d
FU TIANYUAN <e0253737@xgpd3.comp.nus.edu.sg>
2020-11-09 14:03:58 +0800

    file input done

--- PA2-CNN_POS_Tagging/buildtagger.py
+++ PA2-CNN_POS_Tagging/buildtagger.py
@@ -12,7 +12,34 @@ import torch.optim as optim
 def train_model(train_file, model_file):
     # write your code here. You can add functions as well.
 		# use torch library to save model parameters, hyperparameters, etc. to model_file
-    print('Finished...')
+
+    # parse train file
+    words, tags = [], []
+    with open(train_file) as data:
+      for line in data:
+        line_words, line_tags = [], []
+        line = line.strip()
+        wordsntags = line.split(' ')
+        for wordntag in wordsntags:
+          line_tags.append(get_tag(wordntag))
+          line_words.append(get_word(wordntag))
+        words.append(line_words)
+        tags.append(line_tags)
+    print(words[0])
+    print(tags[0])
+    print(len(words), len(tags))
+
+    # build word-to-ix, tag-to-ix
+    
+
+
+
+def get_tag(word):
+    return word.rsplit('/', 1)[1]
+
+def get_word(word):
+    return word.rsplit('/', 1)[0].lower()
+
 		
 if __name__ == "__main__":
     # make no changes here
