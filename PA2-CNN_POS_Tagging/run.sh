afd7a7883f4c7cd9d9556a9b4c1b3159737c022e
Tianyuan <ftygary@gmail.com>
2020-11-20 23:49:30 +0800

    update

--- PA2-CNN_POS_Tagging/run.sh
+++ PA2-CNN_POS_Tagging/run.sh
@@ -1,2 +1,2 @@
-python3 runtagger.py sents.test model output
-python3 eval.py output sents.answer
+python3 runtagger.py pa2_blind_sents.test model output
+python3 eval.py output pa2_blind_sents.answer
